43d8af9a2e980fe20303bd30dc824b5fe20a1f4d
LiaoMingRong <849376723@qq.com>
2020-09-08 13:19:52 +0800

    debug

--- app/Console/Kernal.php
+++ app/Console/Kernal.php
@@ -41,7 +41,7 @@ class Kernal
     public function execCommand($cmd)
     {
         if (substr(php_uname(), 0, 7) == 'Windows') {
-            pclose(popen('start /B '. $cmd, 'r')); 
+            pclose(popen('start /B '.$cmd, 'r')); 
         } else {
             exec($cmd . ' > /dev/null &');
         }
