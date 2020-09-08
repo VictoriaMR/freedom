85ad8260352f7782f4fbad2b3d1db4351344b877
LiaoMingRong <849376723@qq.com>
2020-09-08 13:36:51 +0800

    debug

--- home/index.php
+++ home/index.php
@@ -1,6 +1,7 @@
 <?php
 // 入口文件
 define('APP_MEMORY_START', memory_get_usage());
+ini_set('date.timezone', 'Asia/Shanghai');
 define('APP_TIME_START', microtime(true));
 define('DS', '/');
 define('ROOT_PATH', strtr(realpath(dirname(__FILE__).'/../'), '\\', DS).DS);
